6e2fc8844c00b32d3c42d126bbdd9cdd6b038655
sahinto2 <sahinto2@ncsu.edu>
2022-10-25 17:18:46 -0400

    [Implementation] Created ReportManager to use an encapsulated CompressionManager to do all the processing

--- CompressionManager/src/edu/ncsu/csc316/compression/manager/CompressionManager.java
+++ CompressionManager/src/edu/ncsu/csc316/compression/manager/CompressionManager.java
@@ -2,18 +2,47 @@ package edu.ncsu.csc316.compression.manager;
 
 import java.io.FileNotFoundException;
 
+import edu.ncsu.csc316.compression.dsa.DSAFactory;
+import edu.ncsu.csc316.compression.io.InputReader;
 import edu.ncsu.csc316.dsa.list.List;
 import edu.ncsu.csc316.dsa.map.Map;
 
+/**
+ * CompressionManager is the part of the model that takes a Map from the InputReader and processes it.
+ * The CompressionManager will use the DSAFactory to test an assortment of Map, List, and sorting implementations.
+ * CompressionManager is used by ReportManager to be the encapsulated compressor and decompressor.
+ * @author Sean Hinton (sahinto2)
+ *
+ */
 public class CompressionManager {
 
-
+	/**The underlying map of raw text straight from a file*/
+	private Map<Integer, List<String>> unprocessedMap;
+	/**
+	 * Constructor for the CompressionManager.
+	 * Takes in a file path and creates a file of the unprocessed text.
+	 * It also sets the different Map, List, and Sorting implementations for testing.
+	 * @param pathToInputFile the file path to the File to be read
+	 * @throws FileNotFoundException occurs if the path does not exist
+	 */
     public CompressionManager(String pathToInputFile) throws FileNotFoundException {
-        // TODO Complete this constructor
+        DSAFactory.setMapType(null);
+        DSAFactory.setListType(null);
+        DSAFactory.setComparisonSorterType(null);
+        DSAFactory.setNonComparisonSorterType(null);
+        unprocessedMap = InputReader.readFile(pathToInputFile);
     }
 
+    /**
+     * Compresses the Map of unprocessed text by replacing repeats of words with an Integer.
+     * If a word is repeated it is replaced with the integer of the number where the first occurence was
+     * in the original text.
+     * Maps are returned with Integer as the line number and List of Strings is the line.
+     * @return a Map of compressed text that
+     */
     public Map<Integer, List<String>> getCompressed() {
-        // TODO Complete this method
+        unprocessedMap = DSAFactory.getMap(null);
+        
     }
 
     public Map<Integer, List<String>> getDecompressed() {
--- CompressionManager/src/edu/ncsu/csc316/compression/manager/ReportManager.java
+++ CompressionManager/src/edu/ncsu/csc316/compression/manager/ReportManager.java
@@ -2,19 +2,93 @@ package edu.ncsu.csc316.compression.manager;
 
 import java.io.FileNotFoundException;
 
-public class ReportManager {
+import edu.ncsu.csc316.compression.dsa.Algorithm;
+import edu.ncsu.csc316.compression.dsa.DSAFactory;
+import edu.ncsu.csc316.compression.dsa.DataStructure;
+import edu.ncsu.csc316.dsa.list.List;
+import edu.ncsu.csc316.dsa.map.Map;
 
+/**
+ * ReportManagers is the highest level of the model for the CompressionManager system.
+ * It uses a CompressionManager field to handle processing text lines from a file. Also
+ * depends upon the DSAFactory to construct different types of data structures for testing.
+ * @author Sean Hinton (sahinto2)
+ *
+ */
+public class ReportManager {
+	
+	/**The CompressionManager that returns the compressed or decompressed forms of the maps*/
+	private CompressionManager compressionManager;
+	
+	/**Constant for the indents when returning the compressed or decompressed Maps*/
     private static final String INDENT = "   ";
 
+    /**
+     * Constructor for ReportManager. Takes in a file and constructs the underlying CompressionManager
+     * to read in the same file. It then uses that underlying CompressionManager to do all the work.
+     * Also the location where the sorting algorithm and data structures are set for this class.
+     * @param pathToInputFile the path to a file being read
+     * @throws FileNotFoundException if the specified file path doesn't exist
+     */
     public ReportManager(String pathToInputFile) throws FileNotFoundException {
-        // TODO Complete this constructor
+    	DSAFactory.setMapType(DataStructure.UNORDEREDLINKEDMAP);
+    	DSAFactory.setListType(DataStructure.SINGLYLINKEDLIST);
+    	DSAFactory.setComparisonSorterType(Algorithm.MERGESORT);
+    	DSAFactory.setNonComparisonSorterType(Algorithm.COUNTING_SORT);
+    	
+        compressionManager = new CompressionManager(pathToInputFile);
+        
     }
-
+    /**
+     * This function takes the compressed text from Map form of Integer and List of Strings to a single String.
+     * Should take the form of indent, line #: Strings
+     * @return a String of the compressed Map taken from Map to lines of Strings
+     */
     public String compress() {
-        // TODO Complete this method
+    	Map<Integer, List<String>> compressed = DSAFactory.getMap(null);
+        compressed = compressionManager.getCompressed();
+        //Use a StringBuffer to cut down on appends
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Compressed Output {\n");
+        for(List<String> l : compressed.values()) {
+        	//Now print out each line with an indent
+        	buffer.append(INDENT);
+        	for(int i = 0; i < l.size(); i++) {
+        		
+        		buffer.append(l.get(i));
+        		buffer.append("\n");
+        	}
+        }
+        buffer.append("}");
+        
+        //Now return the compressed output
+        return buffer.toString();
     }
     
+    /**
+     * This function takes the decompressed text from Map to String. Uses the underlying
+     * compressionManager to get the decompressed text and then the Strings in each List value are then
+     * appended to the String.
+     * @return a String of the decompressed Map taken from the Lists of Strings in the Map
+     */
     public String decompress() {
-        // TODO Complete this method
+    	Map<Integer, List<String>> decompressed = DSAFactory.getMap(null);
+        decompressed = compressionManager.getDecompressed();
+        //Use a StringBuffer to cut down on appends
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Compressed Output {\n");
+        for(List<String> l : decompressed.values()) {
+        	//Now print out each line with an indent
+        	buffer.append(INDENT);
+        	for(int i = 0; i < l.size(); i++) {
+        		
+        		buffer.append(l.get(i));
+        		buffer.append("\n");
+        	}
+        }
+        buffer.append("}");
+        
+        //Now return the compressed output
+        return buffer.toString();
     }
 }

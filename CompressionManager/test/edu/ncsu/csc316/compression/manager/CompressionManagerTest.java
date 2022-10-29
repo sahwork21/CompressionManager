f1c71c174a7929f3d783d00a1d3234be9e7e54ed
sahinto2 <sahinto2@ncsu.edu>
2022-10-29 14:54:27 -0400

    [Test] Added testing on the sample txt file

--- CompressionManager/test/edu/ncsu/csc316/compression/manager/CompressionManagerTest.java
+++ CompressionManager/test/edu/ncsu/csc316/compression/manager/CompressionManagerTest.java
@@ -58,6 +58,42 @@ public class CompressionManagerTest {
 		assertEquals("fish", map.get(1).get(1));
 		assertEquals("Two", map.get(1).get(2));
 		assertEquals("fish", map.get(1).get(3));
+		
+		try {
+			cm = new CompressionManager("input/sample.txt");
+		} catch (FileNotFoundException e) {
+			fail("Unable to read");
+			e.printStackTrace();
+		}
+		
+		map = cm.getCompressed();
+		//Now assert everything is right
+		
+		assertEquals("the", map.get(1).get(0));
+		assertEquals("dog", map.get(1).get(1));
+		assertEquals("and", map.get(1).get(2));
+		assertEquals("1", map.get(1).get(3));
+		assertEquals("cat", map.get(1).get(4));
+		assertEquals("3", map.get(1).get(5));
+		assertEquals("1", map.get(1).get(6));
+		assertEquals("fox", map.get(1).get(7));
+		assertEquals("3", map.get(1).get(8));
+		assertEquals("1", map.get(1).get(9));
+		assertEquals("snake", map.get(1).get(10));
+		
+		assertEquals("3", map.get(2).get(0));
+		assertEquals("1", map.get(2).get(1));
+		assertEquals("fish", map.get(2).get(2));
+		assertEquals("3", map.get(2).get(3));
+		assertEquals("1", map.get(2).get(4));
+		assertEquals("horse", map.get(2).get(5));
+		assertEquals("3", map.get(2).get(6));
+		assertEquals("1", map.get(2).get(7));
+		assertEquals("2", map.get(2).get(8));
+		assertEquals("3", map.get(2).get(9));
+		assertEquals("1", map.get(2).get(10));
+		assertEquals("4", map.get(2).get(11));
+		
 	}
 
 }
